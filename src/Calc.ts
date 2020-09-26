6f1be6add17485a182547f5e055f63b461ae7c01
ElectroMorris <gotleibgraf@gmail.com>
2020-09-26 21:37:11 +0300

    Change class main type from Number to String Add methods for all operations in Calc.ts Add buttons in main.ts Add index to connect clicks with methods in index.html Add commentary/note/instruction for usage of "float" (fragmentary number part methods)

--- src/Calc.ts
+++ src/Calc.ts
@@ -2,13 +2,22 @@ export enum Operation {
     SUM,
     SUB,
     DIV,
-    MUL
+    MUL,
+    FLOAT
 }
 
 export default class Calculator {
 
-    private _currentResult = 0;
-
+    private _currentResult = '0';
+    public _stringResult = '';
+    // создаем гет-метод для доступа к переменной результата
+    public get currentResult(): string {
+        return this._currentResult;
+    }
+    // создаем сет-метод для доступа к переменной результата
+    public set currentResult(n: string) {
+        this._currentResult = n;
+    }
     private lastOperation: Operation = null;
 
     public apply(value: number) {
@@ -16,27 +25,28 @@ export default class Calculator {
             // если есть операция -- выполняем её
             switch (this.lastOperation) {
                 case Operation.SUM:
-                    this._currentResult += value;
+                    this._currentResult = (Number(this._currentResult) + value).toString();
                     break;
                 case Operation.SUB:
+                    this._currentResult = (Number(this._currentResult) - value).toString();
                     break;
                 case Operation.DIV:
+                    this._currentResult = (Number(this._currentResult) / value).toString();
                     break;
                 case Operation.MUL:
+                    this._currentResult = (Number(this._currentResult) * value).toString();
                     break;
-
+                case Operation.FLOAT:
+                    if (Number.isInteger(Number(this._currentResult))) this._currentResult += ('.' + value.toString());
+                    else this._currentResult += value.toString();
+                    break;
             }
         } else {
             // иначе просто делаем выбранное число текущим результатом
-            this._currentResult = value;
+            this._currentResult = value.toString();
         }
     }
-
     set operation(value: Operation) {
         this.lastOperation = value;
-    }
-
-    get currentResult() {
-        return this._currentResult;
     }
 }
