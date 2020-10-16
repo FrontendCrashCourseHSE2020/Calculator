a8da1d5d328a61b83c06dcf2599e2959a9664038
rusya1001 <rusya1001@yandex.ru>
2020-10-17 00:10:14 +0300

    2

--- src/Calc.ts
+++ src/Calc.ts
@@ -19,12 +19,14 @@ export default class Calculator {
                     this._currentResult += value;
                     break;
                 case Operation.SUB:
+                    this._currentResult -= value;
                     break;
                 case Operation.DIV:
+                    this._currentResult /= value;
                     break;
                 case Operation.MUL:
+                    this._currentResult *= value;
                     break;
-
             }
         } else {
             // иначе просто делаем выбранное число текущим результатом
